9f7a6cf6edd285ad150bcbb94a5a8895bcd2ca08
Radu Maier <ariademur@webserv.co>
2018-09-26 18:02:28 +0200

    processReleasesExample work

--- public/processReleasesExample.php
+++ public/processReleasesExample.php
@@ -3,19 +3,19 @@ require __DIR__ . '/../vendor/autoload.php';
 
 try {
     //$xmlPath = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1]: null;
-    $xmlPath = __DIR__ . '/../var/data/discogs_20180901_releases.xml.gz';
+    $filePath = __DIR__ . '/../var/data/discogs_20180901_releases.xml.gz';
     $logger = new \WebServCo\Framework\FileLogger(
         'discogs-data',
         __DIR__ . '/../var/log/',
         \WebServCo\Framework\Framework::library('Request')
     );
     $releaseProcessor = new \WebServCo\DiscogsData\ReleasesProcessor();
-    $xmlParser = new \WebServCo\DiscogsData\XmlParser(
+    $dataParser = new \WebServCo\DiscogsData\DataParser(
         $releaseProcessor,
         $logger,
-        $xmlPath
+        $filePath
     );
-    $xmlParser->run();
+    $dataParser->run();
 } catch (\WebServCo\DiscogsData\Exceptions\XmlParserException $e) {
     echo $e->getMessage();
     echo PHP_EOL;
