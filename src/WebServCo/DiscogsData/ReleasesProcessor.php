fd5fa09e1e187c63726fbbe40c46076f9a96b828
Radu Maier <ariademur@webserv.co>
2018-10-01 16:43:27 +0200

    ReleasesProcessor.start,finish, implement progressBar

--- src/WebServCo/DiscogsData/ReleasesProcessor.php
+++ src/WebServCo/DiscogsData/ReleasesProcessor.php
@@ -4,22 +4,40 @@ namespace WebServCo\DiscogsData;
 final class ReleasesProcessor extends AbstractDataProcessor implements
     \WebServCo\DiscogsData\Interfaces\DataProcessorInterface
 {
-    protected $totalItems = 0;
+    protected $progressBar;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->progressBar = new \WebServCo\Framework\ProgressBar(20); //size
+        $this->progressBar->setType('single_line'); //single_line, multi_line
+        $this->progressBar->start(100000); // pb start
+    }
 
     public function getDataType()
     {
         return DataTypes::RELEASE;
     }
 
+    public function start()
+    {
+        echo __METHOD__ . PHP_EOL;
+    }
+
     public function processItem($data)
     {
         ++ $this->totalItems;
 
+        $this->progressBar->advanceTo($this->totalItems); // pb advance
+        echo $this->progressBar->prefix(sprintf('Processing %s/%s', $this->totalItems, 100000));
+
         $outputCheck = $this->totalItems%100;
 
-        if (empty($outputCheck)) {
-            echo sprintf('%s%s', $this->totalItems, PHP_EOL);
-        }
+        //if (empty($outputCheck)) {
+            //echo sprintf('%s%s', $this->totalItems, PHP_EOL);
+        //}
+        $result = true;
+        echo $this->progressBar->suffix($result); // pb suffix
 
         /* *
         var_dump($data->getAttribute('id')); //XXX
@@ -28,5 +46,10 @@ final class ReleasesProcessor extends AbstractDataProcessor implements
         echo PHP_EOL; //XXX
         exit; //XXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
         /* */
+    }
+
+    public function finish()
+    {
+        echo PHP_EOL;
     }
 }
