388fd6e8b536c00c32df935f8f3b91c126ba03c0
Radu Maier <ariademur@webserv.co>
2018-10-01 16:42:42 +0200

    DiscogsData/DataParser initializeDataProcessor

--- src/WebServCo/DiscogsData/DataParser.php
+++ src/WebServCo/DiscogsData/DataParser.php
@@ -6,6 +6,9 @@ use \WebServCo\DiscogsData\Exceptions\DataParserException;
 final class DataParser
 {
     protected $dataProcessor;
+    protected $startCallable;
+    protected $itemCallable;
+    protected $finishCallable;
     protected $logger;
     protected $filePath;
 
@@ -22,9 +25,11 @@ final class DataParser
         if (!is_readable($filePath)) {
             throw new DataParserException(sprintf('File path not readable: %s', $filePath));
         }
-        $this->dataProcessor = $dataProcessor;
-        $this->logger = $logger;
         $this->filePath = $filePath;
+
+        $this->initializeDataProcessor($dataProcessor);
+
+        $this->logger = $logger;
 
         $this->xmlReader = new \XMLReader();
         if (!$this->xmlReader->open(sprintf('compress.zlib://%s', $this->filePath))) {
@@ -35,16 +40,13 @@ final class DataParser
     public function run()
     {
         $dataType = $this->dataProcessor->getDataType();
+        call_user_func_array($this->startCallable, []);
         while ($this->xmlReader->read()) {
             ++ $this->xmlNodeCount;
             if (\XMLReader::ELEMENT == $this->xmlReader->nodeType && $dataType == $this->xmlReader->name) {
                 ++ $this->xmlItemCount;
-                $callable = [$this->dataProcessor, 'processItem'];
-                if (!is_callable($callable)) {
-                    throw new DataParserException('Data processor method not found');
-                }
                 $args = [$this->xmlReader->expand()];
-                $result = call_user_func_array($callable, $args);
+                $result = call_user_func_array($this->itemCallable, $args);
                 if ($result) {
                     ++ $this->xmlProcessedCount;
                 }
@@ -52,6 +54,27 @@ final class DataParser
             }
         }
         $this->xmlReader->close();
+        call_user_func_array($this->finishCallable, []);
         return;
+    }
+
+    protected function initializeDataProcessor($dataProcessor)
+    {
+        $this->dataProcessor = $dataProcessor;
+        if (!is_callable([$this->dataProcessor, 'getDataType'])) {
+            throw new DataParserException('Get data type method not found');
+        }
+        $this->startCallable = [$this->dataProcessor, 'start'];
+        if (!is_callable($this->startCallable)) {
+            throw new DataParserException('Start method not found');
+        }
+        $this->itemCallable = [$this->dataProcessor, 'processItem'];
+        if (!is_callable($this->itemCallable)) {
+            throw new DataParserException('Process item method not found');
+        }
+        $this->finishCallable = [$this->dataProcessor, 'finish'];
+        if (!is_callable($this->finishCallable)) {
+            throw new DataParserException('Finish method not found');
+        }
     }
 }
