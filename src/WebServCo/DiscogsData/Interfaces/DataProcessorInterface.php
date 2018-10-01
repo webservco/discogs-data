1b1951482c7422e76ed5fcb6b44d45711a06eda3
Radu Maier <ariademur@webserv.co>
2018-10-01 16:41:26 +0200

    DataProcessorInterface.start,finish

--- src/WebServCo/DiscogsData/Interfaces/DataProcessorInterface.php
+++ src/WebServCo/DiscogsData/Interfaces/DataProcessorInterface.php
@@ -4,5 +4,7 @@ namespace WebServCo\DiscogsData\Interfaces;
 interface DataProcessorInterface
 {
     public function getDataType();
+    public function start();
     public function processItem($data);
+    public function finish();
 }
