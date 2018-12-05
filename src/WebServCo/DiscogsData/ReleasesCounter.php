d944c63ff7c2a40c82a20361f13318cd6e4a542d
Radu Maier <ariademur@webserv.co>
2018-12-05 09:42:08 +0100

    Cleanup and improve ReleasesCounter, ReleasesProcessor

--- src/WebServCo/DiscogsData/ReleasesCounter.php
+++ src/WebServCo/DiscogsData/ReleasesCounter.php
@@ -4,78 +4,24 @@ namespace WebServCo\DiscogsData;
 final class ReleasesCounter extends AbstractReleasesProcessor implements
     \WebServCo\DiscogsData\Interfaces\DataProcessorInterface
 {
-    protected $progressLine;
-    protected $timeStart;
-    protected $timeFinish;
-
-    public function __construct(\WebServCo\Framework\Interfaces\LoggerInterface $logger)
-    {
-        parent::__construct($logger);
-        $this->progressLine = new \WebServCo\Framework\ProgressLine();
-        $this->progressLine->setShowResult(false);
-    }
-
-    public function start()
-    {
-        $this->timeStart = $this->createCurrentTimeObject();
-        $this->logger->debug(__CLASS__ . PHP_EOL);
-    }
-
     /*
-    * @param mixed $data
+    * @param \DOMElement $domElement
     * @return bool
     */
-    public function processItem($data)
+    public function processItem(\DOMElement $domElement)
     {
-        ++ $this->totalItems;
+        parent::processItem($domElement); // increments counter
 
-        $outputCheck = $this->totalItems%100;
-        if (empty($outputCheck)) { // output every 100 items
+        $outputCheck = $this->totalItems%1000;
+        if (empty($outputCheck)) { // output every 1000 items
             $this->logger->debug(
-                $this->progressLine->prefix(sprintf('Processing item %s', $this->totalItems))
+                $this->progressLine->prefix(sprintf('Processing item %s', $this->totalItems)) // pl prefix
             );
         }
-        $this->logger->debug($this->progressLine->suffix()); // pb suffix
-        return true;
-    }
-
-    public function finish()
-    {
-        $this->timeFinish = $this->createCurrentTimeObject();
-        $this->logger->debug($this->progressLine->finish());
-        $this->showStats();
-    }
-
-    protected function showStats()
-    {
-        $this->logger->debug(sprintf('Total items: %s%s', $this->totalItems, PHP_EOL));
-        $this->logger->debug(
-            sprintf(
-                'Running time: %s seconds (from %s to %s)%s',
-                round($this->timeFinish->format("U.u") - $this->timeStart->format("U.u"), 3),
-                $this->timeStart->format("Y-m-d H:i:s"),
-                $this->timeFinish->format("Y-m-d H:i:s"),
-                PHP_EOL
-            )
-        );
-        $this->logger->debug(
-            sprintf(
-                'Memory peak usage: %s K%s',
-                memory_get_peak_usage(true) / 1024,
-                PHP_EOL
-            )
-        );
-    }
-
-    protected function createCurrentTimeObject()
-    {
-        $dateTime = \DateTime::createFromFormat('U.u', (string) microtime(true));
-        if (!($dateTime instanceof \DateTime)) {
-            throw new \WebServCo\DiscogsData\Exceptions\DataProcessorException(
-                'Error initializing DateTime object'
-            );
+        $result = true;
+        if (empty($outputCheck)) { // output every 1000 items
+            $this->logger->debug($this->progressLine->suffix()); // pl suffix
         }
-        $dateTime->setTimezone(new \DateTimeZone('Europe/Budapest'));
-        return $dateTime;
+        return $result;
     }
 }
--- src/WebServCo/DiscogsData/ReleasesProcessor.php
+++ src/WebServCo/DiscogsData/ReleasesProcessor.php
@@ -4,27 +4,28 @@ namespace WebServCo\DiscogsData;
 final class ReleasesProcessor extends AbstractReleasesProcessor implements
     \WebServCo\DiscogsData\Interfaces\DataProcessorInterface
 {
-    public function start()
-    {
-    }
-
     /*
     * @param mixed $data
     * @return bool
     */
-    public function processItem($data)
+    public function processItem(\DOMElement $domElement)
     {
-        ++ $this->totalItems;
+        parent::processItem($domElement); // increments counter
         /* */
-        var_dump($data->getAttribute('id')); //XXX
-        var_dump($data->getAttribute('status')); //XXX
+        //var_dump($data->getAttribute('id')); //XXX
+        //var_dump($data->getAttribute('status')); //XXX
         //var_dump($data); //XXX
-        echo PHP_EOL; //XXX
-        exit; //XXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-        /* */
-    }
 
-    public function finish()
-    {
+        $outputCheck = $this->totalItems%1000;
+        if (empty($outputCheck)) { // output every 1000 items
+            $this->logger->debug(
+                $this->progressLine->prefix(sprintf('Processing item %s', $this->totalItems)) // pl prefix
+            );
+        }
+        $result = $this->saveXml($domElement); // save XML for each item
+        if (empty($outputCheck)) { // output every 1000 items
+            $this->logger->debug($this->progressLine->suffix($result)); // pl suffix
+        }
+        return $result;
     }
 }
