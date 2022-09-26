355788ef062f6e05bbfcfb87c3fae13fefb01bba
Radu Maier <ariademur@webserv.co>
2022-09-26 10:26:48 +0200

    Code style and logic fixes.

--- examples/discogsData.php
+++ examples/discogsData.php
@@ -1,6 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 use WebServCo\DiscogsData\Exceptions\DiscogsDataException;
+use WebServCo\DiscogsData\Interfaces\DataProcessorInterface;
 use WebServCo\Framework\Cli\Ansi;
 use WebServCo\Framework\Cli\Sgr;
 
@@ -35,9 +38,12 @@ $errorMessages = [
 
 /* Set up */
 
-$type = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : null;
-$processor = isset($_SERVER['argv'][2]) ? $_SERVER['argv'][2] : null;
-$date = isset($_SERVER['argv'][3]) ? $_SERVER['argv'][3]: null;
+// phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable.DisallowedSuperGlobalVariable
+$type = $_SERVER['argv'][1] ?? null;
+// phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable.DisallowedSuperGlobalVariable
+$processor = $_SERVER['argv'][2] ?? null;
+// phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable.DisallowedSuperGlobalVariable
+$date = $_SERVER['argv'][3] ?? null;
 
 /* Output */
 
@@ -46,8 +52,8 @@ $logger->clear();
 $logger->debug(
     Ansi::sgr(
         sprintf('Discogs Data: example: %s %s %s', $type, $processor, $date),
-        [Sgr::BOLD]
-    )
+        [Sgr::BOLD],
+    ),
 );
 
 /* Validation */
@@ -62,7 +68,7 @@ try {
     if (empty($processor)) {
         throw new DiscogsDataException(sprintf($errorMessages['missingParameter'], 'processor (2)'));
     }
-    if (!in_array($processor, $examples[$type])) {
+    if (!in_array($processor, $examples[$type], true)) {
         throw new DiscogsDataException(sprintf($errorMessages['invalidParameter'], 'processor (2)'));
     }
     if (empty($date)) {
@@ -84,19 +90,17 @@ try {
 
     $className = sprintf('\\WebServCo\\DiscogsData\\Processors\\%s\\%s', $type, $processor);
     $dataProcessor = new $className($logger, $outputDirectory);
+    if (!($dataProcessor instanceof DataProcessorInterface)) {
+        throw new DiscogsDataException('Data processor must implement DataProcessorInterface');
+    }
 
     $filePath = sprintf(
         '%svar/data/discogs_%s_%s.xml.gz',
         $projectPath,
         str_replace('-', '', $date),
-        strtolower($type)
+        strtolower($type),
     );
-    $dataParser = new \WebServCo\DiscogsData\Data\Parser(
-        $cliRunner,
-        $dataProcessor,
-        $logger,
-        $filePath
-    );
+    $dataParser = new \WebServCo\DiscogsData\Data\Parser($cliRunner, $dataProcessor, $logger, $filePath);
     $dataParser->run();
 } catch (DiscogsDataException $e) {
     $logger->error(sprintf('Error: %s%s', $e->getMessage(), PHP_EOL));
